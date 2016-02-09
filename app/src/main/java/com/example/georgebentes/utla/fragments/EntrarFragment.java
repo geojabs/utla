689478a7ebe924b79ab84d9ec534c4f1c0241f18
George Bentes <george.bentes@gmail.com>
2016-02-08 22:29:07 -0400

    - Comitando o fragment para logar no app; - Achei melhor separar ele do fragmente de tela inicial. Fica melhor para dar manutenção no futuro e deixa o código mais limpo. - Este fragmento não é chamado dinamicamente por nenhuma outra classe. Ele foi adicionado manualmente mesmo no arquivo xml da tela inicial fragment.

---
+++ app/src/main/java/com/example/georgebentes/utla/fragments/EntrarFragment.java
@@ -0,0 +1,30 @@
+package com.example.georgebentes.utla.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.georgebentes.utla.R;
+
+/**
+ * Created by George Bentes on 08/02/2016.
+ */
+public class EntrarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_entrar, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
